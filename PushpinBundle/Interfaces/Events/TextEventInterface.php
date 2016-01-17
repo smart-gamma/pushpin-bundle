69f14ff74fe2dd5a54e0038b51af2318fdb377cc
Stas Zozulja <stas.z@smart-gamma.com>
2016-01-17 14:29:31 +0200

    interfaces

---
+++ PushpinBundle/Interfaces/Events/TextEventInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Gamma\Pushpin\PushpinBundle\Interfaces;
+
+interface TextEventInterface
+{
+    /**
+     * @return string
+     */
+    public function getText();
+}
---
+++ PushpinBundle/Interfaces/Handlers/EventHandlerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gamma\Pushpin\PushpinBundle\Interfaces\Handlers;
+
+use GripControl\WebSocketEvent;
+
+interface EventHandlerInterface
+{
+    /**
+     * @param WebSocketEvent $webSocketEvent
+     *
+     * @return WebSocketEvent
+     */
+    public function handle(WebSocketEvent $webSocketEvent);
+
+    /**
+     * @return string
+     */
+    public function getEventName();
+
+    /**
+     * @param WebSocketEvent $webSocketEvent
+     *
+     * @return bool
+     */
+    public static function eventSupported(WebSocketEvent $webSocketEvent);
+}
