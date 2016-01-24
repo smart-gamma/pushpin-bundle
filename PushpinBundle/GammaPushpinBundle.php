1fcd29f60f01e6140ccd330a68655045eeb91c89
Stas Zozulja <stas.z@smart-gamma.com>
2016-01-24 19:36:43 +0200

    fix compile pass

--- PushpinBundle/GammaPushpinBundle.php
+++ PushpinBundle/GammaPushpinBundle.php
@@ -2,8 +2,16 @@
 
 namespace Gamma\Pushpin\PushpinBundle;
 
+use Gamma\Pushpin\PushpinBundle\DependencyInjection\AddHandlersCompilePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GammaPushpinBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AddHandlersCompilePass());
+    }
 }
